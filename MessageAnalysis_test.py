03eebc2c287133600729da9e34f07667a92dcb59
kyleyeh <kyle.yeh@d11.org>
2025-10-05 09:54:58 -0600

    Update MessageAnalysis_test.py

--- MessageAnalysis_test.py
+++ MessageAnalysis_test.py
@@ -47,20 +47,20 @@ def test_countByReceiver3():
 
 def test_getMessageByID1():
     filename = "interceptedMessages.txt"
-    id = 70360314
+    id = "70360314"
     assert StudentCode.getMessageByID(filename, id) == "Lighthearted note: The cafeteria droids are still terrible cooks."
 
 def test_getMessageByID2():
     filename = "interceptedMessages.txt"
-    id = 78920329
+    id = "78920329"
     assert StudentCode.getMessageByID(filename, id) == "Encrypted directive: Resource allocation to Eadu confirmed."
 
 def test_getMessageByID3():
     filename = "interceptedMessages.txt"
-    id = 14170814
+    id = "14170814"
     assert StudentCode.getMessageByID(filename, id) == "Encrypted update: Stardust nearing completion. Security is paramount."
 
 def test_getIDByMessage():
     filename = "interceptedMessages.txt"
     message = "I have friends everywhere."
-    assert StudentCode.getIDByMessage(filename, message) == 94031003
+    assert StudentCode.getIDByMessage(filename, message) == "94031003"
